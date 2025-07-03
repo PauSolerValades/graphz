0f3dbc975b804ee06b4f34ed67784b6e0106f23a
PauSolerValades <pau.soler.valades@gmail.com>
2025-07-03 11:21:08 +0200

    Node insertion done

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,12 @@
 const std = @import("std");
 const Graph = @import("graphz_lib");
 
-pub fn main() void {
+pub fn main() !void {
     std.debug.print("Hello Grpah", .{});
     const allocator = std.heap.page_allocator;
-    _ = Graph.Graph(u16, f32).init(allocator);
+    var graph = Graph.Graph(u16, f32).init(allocator);
+    
+    try graph.newNode(2);
+    try graph.newNode(2);
 
 }
--- src/root.zig
+++ src/root.zig
@@ -1,12 +1,14 @@
-const std = @import("std");
+const std = @import("std"); 
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 const AutoHashMap = std.AutoHashMap;
 const HashMap = std.HashMap;
+const CAPACITY = 64;
 
 pub const GraphError = error {
     NodeNotFound,
     EdgeNotFound,
+    RepeatedNodeInsertion,
 };
 
 pub fn Graph(comptime T: type, comptime W: type) type {
@@ -19,7 +21,7 @@ pub fn Graph(comptime T: type, comptime W: type) type {
 
         const Node = struct {
             value: T,
-            nh: ArrayList(Edge),
+            nh: std.ArrayListAligned(Edge, null),
         };
 
         const Edge = struct {
@@ -34,8 +36,24 @@ pub fn Graph(comptime T: type, comptime W: type) type {
                 .nodes = AutoHashMap(T, Node).init(allocator),                
             };
         }
+        
+        pub fn newNode(self: *Self, value: T) !void {
+            
+            if(self.nodes.contains(value)) {
+                std.debug.print("No repeated values are allowed\n", .{});
+                return GraphError.RepeatedNodeInsertion;
+            } 
+            const node_ptr: *Node = try self.allocator.create(Node);
 
+            node_ptr.* = .{
+               .value = value,
+               .nh = try ArrayList(Edge).initCapacity(self.allocator, CAPACITY), 
+            };
 
+            try self.nodes.put(value, node_ptr.*);
+
+            return;
+        }
     };
 }
 
