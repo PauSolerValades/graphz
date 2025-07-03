3a3d5b818c2da803aa6ce774f6561efb0682f032
PauSolerValades <pau.soler.valades@gmail.com>
2025-07-03 10:47:32 +0200

    Real first commit

--- src/main.zig
+++ src/main.zig
@@ -1,1 +1,9 @@
+const std = @import("std");
+const Graph = @import("graphz_lib");
 
+pub fn main() void {
+    std.debug.print("Hello Grpah", .{});
+    const allocator = std.heap.page_allocator;
+    _ = Graph.Graph(u16, f32).init(allocator);
+
+}
